fbce5c2feef3c8ce76759813feebe34873a21c81
boundles <wangyang9113@gmail.com>
2023-02-23 16:34:24 +0800

    update server

--- tools/server_cli.py
+++ tools/server_cli.py
@@ -5,12 +5,14 @@ from flask import Flask
 
 from aiq.dataset import Alpha158
 from aiq.models import XGBModel
+from aiq.utils.logging import get_logger
 
 from ais.strategies.signal_strategy import TopkDropoutStrategy
 from ais.data.dataset import Dataset
 
 
 app = Flask(__name__)
+logger = get_logger('Aiq Server')
 
 # load model
 model = XGBModel()
@@ -20,22 +22,19 @@ model.load('/home/zcs/darrenwang/aiq/checkpoints')
 strategy = TopkDropoutStrategy()
 
 
-def get_prediction(dataset):
-    result = model.predict(dataset).to_dataframe()
-    return result
-
-
 @app.route("/predict/date=<date>")
 def predict(date):
+    logger.info('input request: %s' % date)
     # input data
     start_time = datetime.datetime.strftime(
         datetime.datetime.strptime(date, '%Y-%m-%d') - datetime.timedelta(days=100), '%Y-%m-%d')
     end_time = date
-    dataset = Dataset('000300.SH', start_time=start_time, end_time=end_time, min_periods=60,
+    dataset = Dataset(instruments='000300.SH', start_time=start_time, end_time=end_time, min_periods=60,
                       handler=Alpha158(test_mode=True), shuffle=False)
+    logger.info('predict %d items' % dataset.to_dataframe().shape[0])
 
     # predict
-    prediction_result = get_prediction(dataset)
+    prediction_result = model.predict(dataset).to_dataframe()
 
     # response
     buy_order_list, keep_order_list, sell_order_list = strategy.generate_trade_decision(prediction_result)
