e58e55154aaff10c41007096901bb946273bbf4f
boundles <wangyang9113@gmail.com>
2023-05-25 00:07:37 +0800

    fix test

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,8 +1,9 @@
-from aiq.dataset.handler import Alpha158
+from aiq.dataset.handler import Alpha158, Alpha101
 
 from ais.data.dataset import Dataset
 
 
 if __name__ == '__main__':
-    dataset = Dataset('000300.SH', start_time='2021-09-20', end_time='2022-02-20', handler=Alpha158(test_mode=True))
+    handlers = (Alpha158(test_mode=True), Alpha101(test_mode=True))
+    dataset = Dataset('000300.SH', start_time='2021-09-20', end_time='2022-02-20', handlers=handlers)
     print(dataset.to_dataframe().head(1))
